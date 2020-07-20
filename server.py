90be8b225f93cec4d6deb819787ad7e82a0ffe8a
Piyush Gautam <63811393+17Piyush@users.noreply.github.com>
2020-07-20 17:45:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -12,26 +12,25 @@ def html_page(page_name):
     return render_template(page_name)
 
 def write_to_file(data):
-	with open('database.txt', mode='a') as database:
-		email = data["email"]
-		subject = data["subject"]
-		message = data["message"]
-		file = database.write(f'\nEmail:{email}, Subject:{subject}, Message:{message}')
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\nEmail:{email}, Subject:{subject}, Message:{message}')
 
 def write_to_csv(data):
-	with open('database.csv', mode='a') as database2:
-		email = data["email"]
-		subject = data["subject"]
-		message = data["message"]
-		csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-		csv_writer.writerow([email,subject,message])
-
+    with open('database.csv', mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
 
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
-	    data = request.form.to_dict()
-	    write_to_csv(data)
-	    return redirect('./thankyou.html')
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('./thankyou.html')
     else:
-    	return 'Something went wrong. Try again!'
+        return 'Something went wrong. Try again!'
